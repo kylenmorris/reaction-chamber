1060682bf0bde4c56190a54ac1ed9ada3cb4e37c
Kylen Morris (gth842) <kylen.morris@usask.ca>
2026-01-15 21:20:57 -0600

    forgotten zero..

--- src/blink/controller/tube_optical_ctrl.c
+++ src/blink/controller/tube_optical_ctrl.c
@@ -14,7 +14,7 @@ void tube_optical_ctrl_step(void) {
     // Read optical intensities into gOpticalInputs here
     for (int i = 0; i < NUM_TUBES; i++) {
                                                                               
-        gOpticalInputs.intensity[i] = (OPTICAL_REACTION_THRESHOLD + 100 - rand() % 100);
+        gOpticalInputs.intensity[i] = (OPTICAL_REACTION_THRESHOLD + 100 - rand() % 1000);
     }
 }
 

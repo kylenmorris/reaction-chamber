bc5e8222e65d5449ccac2436d4aebc6a4dd33838
Kylen Morris (gth842) <kylen.morris@usask.ca>
2026-01-15 21:20:19 -0600

    way simpler calculation

--- src/blink/controller/tube_optical_ctrl.c
+++ src/blink/controller/tube_optical_ctrl.c
@@ -14,9 +14,7 @@ void tube_optical_ctrl_step(void) {
     // Read optical intensities into gOpticalInputs here
     for (int i = 0; i < NUM_TUBES; i++) {
                                                                               
-        gOpticalInputs.intensity[i] = (OPTICAL_REACTION_THRESHOLD +                  // base level
-                                        (rand() % OPTICAL_REACTION_THRESHOLD / 10) ) // just above threshold    
-                                      * ((rand() % 100) - 99);                              // usually zero   
+        gOpticalInputs.intensity[i] = (OPTICAL_REACTION_THRESHOLD + 100 - rand() % 100);
     }
 }
 

75f238827c445d50b35f3d3b0d3a4a862176c625
Kylen Morris (gth842) <kylen.morris@usask.ca>
2026-01-24 16:21:41 -0600

    Small fixes for moving between testing and flashing, and script to run tests

---
+++ src/blink/clean_build.ps1
@@ -0,0 +1,13 @@
+# clean_build.ps1
+
+# Stop if any command fails
+$ErrorActionPreference = "Stop"
+
+cd C:\Users\kylen\gth842\capstone\pathoscan\src\blink
+
+rmdir build -Force -Recurse
+rmdir build_tests -Force -Recurse
+
+mkdir build
+mkdir build_tests
+

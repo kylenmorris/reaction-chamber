519d8598cfcd68827b374ef1d539dbe0e1769e45
Kylen Morris (gth842) <kylen.morris@usask.ca>
2026-02-01 12:09:38 -0600

    cleanup

--- code/src/controllers/sd_ctrl.c
+++ code/src/controllers/sd_ctrl.c
@@ -4,23 +4,20 @@
 #include "sd_drv.h"
 #include <stdlib.h>
 
-
-void save_test_results_to_file(char* filename) {
+void save_test_result_from_global_to_filename(char* filename) {
     char *test_results = serialize_test_results();
     save_to_file(filename, test_results);
 
     free(test_results);
 }
 
-
-void load_all_filenames(void) {
-
+void load_all_sd_filenames_into_global(void) {
+    populate_file_list("");
 }
 
-
-void load_test_from_filename(char* filename) {
+void load_test_result_from_filename_into_global(char* filename) {
     char *test_string = load_file(filename);
-
     read_json_string(test_string);
+    
     free(test_string);
 }

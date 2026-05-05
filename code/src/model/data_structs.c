658ef3a425cf429d251eb96c291d42e0a6ff435b
Kylen Morris (gth842) <kylen.morris@usask.ca>
2026-05-05 10:58:52 -0600

    updated values

---
+++ code/src/model/data_structs.c
@@ -0,0 +1,120 @@
+#include "data_structs.h"
+#include <stdint.h>
+
+bool DEBUG_MODE = false;
+
+// ####################################
+// STRUCTS
+// ####################################
+
+SystemInfo gSystemInfo = {
+    .temp_sensor_success_count = 0,
+    .optical_sensor_success_count = 0,
+    .latest_filename_int = 0,
+    .led_state = false
+};
+
+SystemError gSystemError = {
+    .current_error = ERROR_NONE,
+};
+
+SystemState gSystemState = BOOT;
+
+TestManagerState gTestManagerState = TEST_IDLE;
+
+SystemControl gSysControl = {
+    .tube_present = { false }
+};
+
+TestStatus gTestStatus = {
+    .tubes = { },
+    .reaction_start_time = 0,
+    .reaction_total_time = 0,
+    .reaction_active = false,
+    .test_invalid = false,
+    .completed = false
+};
+
+HeaterState gHeaterState = {
+    .heaterOn = false
+};
+
+ButtonInput gButtonInput = {
+    .lastPressed = -1,
+    .wasPressed = false,
+    .buttonStates = { HIGH, HIGH, HIGH, HIGH },
+    .lastButtonStates = { HIGH, HIGH, HIGH, HIGH },
+    .steadyTime = { 0, 0, 0, 0 }
+};
+
+OpticalInputs gOpticalInputs = {
+    .intensity = { 0.0f }
+};
+
+TempStatus gTempStatus = {
+    .chamber_temp = 0.0f,
+    .temp_low = false,
+    .temp_extreme = false,
+    .target_temp = TEMP_DESIRED_C
+};
+
+SimParams gSimParams = {
+    .ambient_temp = 0.0f,
+    .heat_rate = 0.3f,
+    .cool_rate = 0.1f,
+    .force_temp_sensor_fault = false
+};
+
+uint16_t gSimTime = 10;
+
+// ####################################
+// IMODEL STRUCTS
+// ####################################
+
+ErrorMenuIM gErrorMenuIM = {
+    .last_redraw = 0,
+    .needs_redraw = true
+};
+
+debugMenuIM gDebugMenuIM = {
+    .needs_redraw = true,
+    .last_redraw = 0,
+    .selected_index = 0
+};
+
+idleMenuIM gIdleMenuIM = {
+    .needs_redraw = true,
+    .last_redraw = 0,
+
+    .selected_index = 0
+};
+
+heatingMenuIM gHeatingMenuIM = {
+    .needs_redraw = true,
+    .last_redraw = 0
+};
+
+testRunningIM gTestRunningIM = {
+    .needs_redraw = true,
+    .last_redraw = 0
+};
+
+resultsIM gResultsIM = {
+    .needs_redraw = true,
+    .last_redraw = 0,
+};
+
+// Populated by sd_drv.c
+char results_menu_items[MAX_FILES][MAX_NAME_LEN];
+
+historyIM gHistoryIM = {
+    .needs_redraw = true,
+    .last_redraw = 0,
+
+    .selected_index = 0,
+    .scroll_index = 0,
+    .selected_index_relative = 0,
+    .num_items = 0
+};
+
+bool filenames_need_update = true;
